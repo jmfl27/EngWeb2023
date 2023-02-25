cee5923a6459c6425a884d1bf6e63762e0b69ad8
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2023-02-25 20:48:14 +0000

    inicio do tpc2

---
+++ TPC2/servidor.js
@@ -0,0 +1,23 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+http.createServer(function (req,res){
+    var pedido = url.parse(req.url,true).pathname
+
+    var d = new Date().toISOString().substring(0, 16)
+
+    console.log(req.method + " " + req.url + " " + d)
+
+    fs.readFile('pag' + pedido.substring(1) + '.html', function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'})
+        if(err){
+            res.write("Erro na leitura do ficheiro: " + err)
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+}).listen(7778)
+
+console.log("Servidor à escuta na porta 7778...")
