e6466b7a998ed3b1c190a118b2d5d1959db837ad
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2023-03-05 00:45:26 +0000

    paginas de distribuição por sexo feitas

--- TPC3/server.js
+++ TPC3/server.js
@@ -7,20 +7,24 @@ http.createServer(function (req, res) {
     var d = new Date().toISOString().substring(0, 16)
     console.log(req.method + " " + req.url + " " + d)
 
+    // PAGINA INCIAL
     if(req.url == '/'){
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
         res.write(mypages.genIndex(d))
         res.end()
     }
+    // LISTA DE PESSOAS (ASCENDENTE)
     else if(req.url == '/pessoas'){
-        // nao ordenado:  axios.get('http://localhost:3000/pessoas')
-        axios.get('http://localhost:3000/pessoas?_sort=nome&_order=asc') //pode ser 'desc'
+        axios.get('http://localhost:3000/pessoas')
             .then(function(resp){
                 var pessoas = resp.data
-                
+                let pessoasOrdenadas = pessoas.sort(
+                    (p1, p2) => (p1.nome < p2.nome) ? -1 : 1
+                )
+                      
                 console.log("Recuperei " + pessoas.length + " registos")       
                 res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-                res.end(mypages.genListaPes(pessoas, d))
+                res.end(mypages.genListaPes(pessoasOrdenadas, d))
             })
             .catch(erro => {
                 console.log("Erro: " + erro)
@@ -28,20 +32,7 @@ http.createServer(function (req, res) {
                 res.end("<p>ERRO: " + erro + "</p>")
             })
     }
-    else if(req.url.match(/p\d+/)){ // expressao regular de p(int)
-        axios.get('http://localhost:3000/pessoas/' + req.url.substring(9))
-            .then(function(resp){
-                var pessoa = resp.data
-                
-                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-                res.end(mypages.genPessoaPage(pessoa, d))
-            })
-            .catch(erro => {
-                console.log("Erro: " + erro)
-                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-                res.end("<p>ERRO: " + erro + "</p>")
-            })
-    }
+    // LISTA DE PESSOAS (DESCENDENTE)
     else if(req.url == '/ordDesc'){
         axios.get('http://localhost:3000/pessoas')
             .then(function(resp){
@@ -59,7 +50,23 @@ http.createServer(function (req, res) {
                 res.end("<p>ERRO: " + erro + "</p>")
             })
     }
-    else if(req.url == '/w3.css'){
+    // PAGINA DE PESSOA EM ESPECIFICO
+    else if(req.url.match(/p\d+/)){ // expressao regular de p(int)
+        axios.get('http://localhost:3000/pessoas/' + req.url.substring(9))
+            .then(function(resp){
+                var pessoa = resp.data
+                
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.end(mypages.genPessoaPage(pessoa, d))
+            })
+            .catch(erro => {
+                console.log("Erro: " + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.end("<p>ERRO: " + erro + "</p>")
+            })
+    }
+    // CSS
+    else if(req.url.match('/w3.css')){
         fs.readFile('w3.css', function(err, data) {
             res.writeHead(200, {'Content-Type': 'text/css'})
             if(err){
@@ -71,6 +78,41 @@ http.createServer(function (req, res) {
             res.end()
         })
     }
+    // PÁGINA DE DISTRIBUIÇÃO POR SEXO
+    else if(req.url == '/sexo'){
+        axios.get('http://localhost:3000/pessoas')
+            .then(function(resp){
+                var pessoas = resp.data
+                let pessoasOrdenadas = pessoas.sort(
+                    (p1, p2) => (p1.nome < p2.nome) ? 1 : -1
+                )
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.end(mypages.genPagDistSexo(pessoasOrdenadas, d))
+            })
+            .catch(erro => {
+                console.log("Erro: " + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.end("<p>ERRO: " + erro + "</p>")
+            })
+    }
+    else if(req.url.match(/sexo\/\w+/)){
+        axios.get('http://localhost:3000/pessoas')
+            .then(function(resp){
+                var pessoas = resp.data
+                let pessoasOrdenadas = pessoas.sort(
+                    (p1, p2) => (p1.nome < p2.nome) ? 1 : -1
+                )
+                var sexo = req.url.substring(6)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.end(mypages.genPagSexoEscolhido(sexo,pessoasOrdenadas, d))
+            })
+            .catch(erro => {
+                console.log("Erro: " + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.end("<p>ERRO: " + erro + "</p>")
+            })
+    }
+    // ERRO
     else{
         res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})
         res.end("<p>ERRO: Operação não suportada...</p>")
