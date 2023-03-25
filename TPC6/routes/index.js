23d9bb709a1a28e8c5d6fb2cbf339fafd9c41b52
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2023-03-25 22:08:22 +0000

    rest api feita

--- TPC6/routes/index.js
+++ TPC6/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Pessoa = require('../controllers/pessoa')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/pessoas', function(req, res, next) {
+  Pessoa.list()
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(601).json({erro : erro}))
 });
+
+router.get('/pessoas/:id', function(req, res, next) {
+  Pessoa.getPessoa(req.params.id)
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+router.post('/pessoas', (req,res) => {
+  Pessoa.addPessoa(req.body)
+    .then(dados => res.status(201).json(dados))
+    .catch(erro => res.status(603).json({erro : erro}))
+})
+
+router.put('/pessoas/:id', (req,res) => {
+  Pessoa.updatePessoa(req.body)
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(604).json({erro : erro}))
+})
+
+router.delete('/pessoas/:id', (req,res) => {
+  Pessoa.deletePessoa(req.params.id)
+    .then(dados => res.json(dados))
+    .catch(erro => res.json({erro : erro}))
+})
 
 module.exports = router;
