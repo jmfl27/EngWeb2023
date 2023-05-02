5ac5af26bde142e0eceb56d26fec098200d0f123
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2023-05-02 10:52:48 +0100

    afer 2

---
+++ Aferição/API/models/emd.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose')
+
+var nomeSchema = new mongoose.Schema({
+    primeiro: String,
+    último: String,
+    _id: String
+})
+
+var emdSchema = new mongoose.Schema({
+    _id: String,
+    index : Number,
+    dataEMD: String,
+    nome: {
+        type: nomeSchema,
+        required: false
+    },
+    idade: Number,
+    género: String,
+    morada: String,
+    modalidade: String,
+    clube: String,
+    email: String,
+    federado: Boolean,
+    resultado: Boolean
+})
+
+module.exports = mongoose.model('exames', emdSchema)
---
+++ Aferição/API/routes/index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+var EMD = require('../controllers/emd')
+
+/* GET home page. */
+/*
+router.get('/EMDs', function(req, res, next) {
+  EMD.list()
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(601).json({erro : erro}))
+});
+
+router.get('/EMDs/:id', function(req, res, next) {
+  EMD.getEMD(req.params.id)
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+*/
+
+// API: 1
+router.get('/api/emd', function(req, res, next) {
+  EMD.getEMDList()
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+// API: 2
+router.get('/api/emd/:id', function(req, res, next) {
+  EMD.getEMD(req.params.id)
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+// API: 3
+router.get('/api/modalidades', function(req, res, next) {
+  EMD.getModalidades()
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+// API: 4
+router.get('/api/emd?res=OK', function(req, res, next) {
+  EMD.getAprovados()
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+// API: 5
+router.get('/api/emd?modalidade=:modal', function(req, res, next) {
+  EMD.getXModalidade(req.params.modal)
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+// API: 6
+router.get('/api/atletas?gen=F', function(req, res, next) {
+  EMD.getFemininos()
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+// API: 7
+router.get('/api/emd?modalidade=:clube', function(req, res, next) {
+  EMD.getXClube(req.params.clube)
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(602).json({erro : erro}))
+});
+
+/*
+
+router.post('/EMDs', (req,res) => {
+  EMD.addEMD(req.body)
+    .then(dados => res.status(201).json(dados))
+    .catch(erro => res.status(603).json({erro : erro}))
+})
+
+router.put('/EMDs/:id', (req,res) => {
+  EMD.updateEMD(req.body)
+    .then(dados => res.json(dados))
+    .catch(erro => res.status(604).json({erro : erro}))
+})
+
+router.delete('/EMDs/:id', (req,res) => {
+  EMD.deleteEMD(req.params.id)
+    .then(dados => res.json(dados))
+    .catch(erro => res.json({erro : erro}))
+})
+*/
+
+module.exports = router;
